3023c663b1177376f6cbf04ceb50832966596cfd
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-17 11:25:20 -0300

    Updated Dynamic's Header Logo

---
+++ src/svg/DynamicLogo.js
@@ -0,0 +1,230 @@
+import React from 'react'
+
+const DynamicLogo = () => (
+  <svg overflow="hidden" viewBox="0 0 673 157">
+      <defs>
+        <linearGradient
+          id="prefix__d"
+          x1={-32.024}
+          x2={770.39}
+          y1={441.2}
+          y2={195.28}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#242F65" offset={0} />
+          <stop stopColor="#3B5F9E" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__e"
+          x1={-17.657}
+          x2={784.75}
+          y1={488.08}
+          y2={242.16}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#242F65" offset={0} />
+          <stop stopColor="#3B5F9E" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__f"
+          x1={-12.648}
+          x2={789.76}
+          y1={504.44}
+          y2={258.51}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#242F65" offset={0} />
+          <stop stopColor="#3B5F9E" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__g"
+          x1={-4.229}
+          x2={798.18}
+          y1={531.89}
+          y2={285.98}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#242F65" offset={0} />
+          <stop stopColor="#3B5F9E" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__h"
+          x1={4.449}
+          x2={806.86}
+          y1={560.22}
+          y2={314.3}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#242F65" offset={0} />
+          <stop stopColor="#3B5F9E" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__i"
+          x1={8.139}
+          x2={810.55}
+          y1={572.23}
+          y2={326.32}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#242F65" offset={0} />
+          <stop stopColor="#3B5F9E" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__j"
+          x1={16.907}
+          x2={819.32}
+          y1={600.85}
+          y2={354.93}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#242F65" offset={0} />
+          <stop stopColor="#3B5F9E" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__k"
+          x1={379.58}
+          x2={435.66}
+          y1={203.91}
+          y2={259.98}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#242F65" offset={0} />
+          <stop stopColor="#3B5F9E" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__l"
+          x1={406.79}
+          x2={460.45}
+          y1={312.1}
+          y2={258.44}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#3B5F9E" offset={0} />
+          <stop stopColor="#5984B4" offset={0.93} />
+          <stop stopColor="#5B87B5" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__m"
+          x1={331.81}
+          x2={301.72}
+          y1={253.16}
+          y2={253.16}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#242F65" offset={0} />
+          <stop stopColor="#3B5F9E" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__n"
+          x1={332.87}
+          x2={351.81}
+          y1={244.7}
+          y2={244.7}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#3B5F9E" offset={0} />
+          <stop stopColor="#5B87B5" offset={1} />
+        </linearGradient>
+        <linearGradient
+          id="prefix__o"
+          x1={400.05}
+          x2={367.14}
+          y1={236.31}
+          y2={236.31}
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stopColor="#242F65" offset={0} />
+          <stop stopColor="#3B5F9E" offset={1} />
+        </linearGradient>
+        <clipPath id="prefix__a">
+          <path
+            d="M296 383h673v157H296z"
+            clipRule="evenodd"
+            fillRule="evenodd"
+          />
+        </clipPath>
+        <clipPath id="prefix__b">
+          <path
+            d="M297 383h671v156H297z"
+            clipRule="evenodd"
+            fillRule="evenodd"
+          />
+        </clipPath>
+        <clipPath id="prefix__c">
+          <path
+            d="M297 383h671v156H297z"
+            clipRule="evenodd"
+            fillRule="evenodd"
+          />
+        </clipPath>
+      </defs>
+      <g transform="translate(-296 -383)" clipPath="url(#prefix__a)">
+        <g clipPath="url(#prefix__b)">
+          <g clipPath="url(#prefix__c)">
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M100.15 344.76h32.424c10.998 0 30.565 0 43.272 18.266 6.749 9.259 8.999 19.717 8.999 30.435 0 26.725-13.238 49.221-50.421 49.221H100.15zm24.996 75.986h10.718c18.656 0 23.995-12.998 23.995-26.865 0-5.549-1.19-12.568-4.759-18.127-2.909-4.359-7.938-8.998-19.056-8.998h-10.878z"
+              fill="url(#prefix__d)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M266.6 369.24l-36.783 97.922h-23.285l11.637-28.195-27.205-69.727h23.426l14.687 42.602h.26l13.998-42.602z"
+              fill="url(#prefix__e)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M274.67 369.24h20.506v7.809h.26c2.25-3.58 6.889-10.328 19.996-10.328 6.999 0 14.818 2.919 18.787 6.359 3.969 3.439 8.468 9.258 8.468 23.685v45.912h-21.996v-40.093c0-5.159 0-15.217-11.508-15.217-12.577 0-12.577 10.848-12.577 15.347v39.993H274.61z"
+              fill="url(#prefix__f)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M435.42 442.68h-21.996v-7.939h-.26c-3.709 7.149-12.838 10.458-21.436 10.458-23.286 0-36.913-18.266-36.913-39.303 0-24.085 17.327-39.172 36.913-39.172 11.998 0 18.787 5.829 21.436 10.328h.26v-7.809h21.996zm-40.353-17.997c13.758 0 18.387-11.648 18.387-18.787 0-8.868-6.079-18.656-18.517-18.656-12.437 0-18.126 10.578-18.126 18.916 0 6.879 5.019 18.527 18.256 18.527z"
+              fill="url(#prefix__g)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M451.43 369.24h20.486v7.809h.26c1.73-3.05 6.489-10.328 20.246-10.328 6.489 0 15.357 2.259 21.176 13.107 4.5-7.148 11.998-13.107 23.286-13.107 5.159 0 13.367 1.199 19.316 7.408 6.889 7.279 7.939 12.178 7.939 28.455v40.093h-21.996v-38.993c0-5.999 0-16.277-11.248-16.277-12.178 0-12.178 11.118-12.178 15.217v40.093h-21.996v-39.993c0-3.999 0-15.347-10.997-15.347-12.438 0-12.438 11.908-12.438 16.277v38.993H451.29z"
+              fill="url(#prefix__h)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M601.59 361.43h-21.996v-16.677h21.996zm0 81.245h-21.996v-73.437h21.996z"
+              fill="url(#prefix__i)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M691.7 414.75c-5.061 21.187-26.34 34.26-47.527 29.198-17.694-4.226-30.202-20.005-30.278-38.197 0-21.576 16.797-38.992 38.763-38.992 19.456 0 35.203 12.837 38.902 30.834h-22.226c-2.379-4.999-6.748-10.318-15.877-10.318-10.458-.53-17.466 8.068-17.466 18.656s6.998 18.657 17.466 18.657c9.129 0 13.498-5.299 15.877-9.798z"
+              fill="url(#prefix__j)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M492.96 236.31c-.032 50.613-41.089 91.616-91.702 91.584a91.607 91.607 0 01-18.507-1.901V287.85c28.462 10.29 59.877-4.441 70.167-32.903s-4.442-59.877-32.904-70.167a54.794 54.794 0 00-36.813-.16l-.45-37.993c49.536-10.276 98.023 21.55 108.3 71.086a91.616 91.616 0 011.909 18.587z"
+              fill="url(#prefix__k)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M492.96 236.31c-.032 50.613-41.089 91.616-91.702 91.584a91.607 91.607 0 01-18.507-1.901V287.85c28.465 10.282 59.875-4.459 70.157-32.924a54.807 54.807 0 003.259-18.616"
+              fill="url(#prefix__l)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M298.94 236.25h33.824v33.824c-18.68 0-33.824-15.144-33.824-33.824z"
+              fill="url(#prefix__m)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M332.72 219.34h33.824v50.731H332.72z"
+              fill="url(#prefix__n)"
+            />
+            <path
+              transform="matrix(1 0 0 1.0009 244.21 53.644)"
+              d="M366.43 202.54h33.823v67.538H366.43z"
+              fill="url(#prefix__o)"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
+)
+
+export default DynamicLogo
