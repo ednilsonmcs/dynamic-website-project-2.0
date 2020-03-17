ad2229c0f1add4de65bd2332d0dffe568b6b1086
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-17 13:02:06 -0300

    New Component Section & Responsive 1st Section

---
+++ src/components/SolucoesSection.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import Card from './Card';
+import DevIcon from '../svg/DevIcon';
+import BulbIcon from '../svg/BulbIcon';
+import ClassIcon from '../svg/ClassIcon';
+
+const SolucoesSection = () => (
+  <section id="solucoes" className="my-20 py-16 pb-20 lg:mb-40 lg:mt-48" style={{background: "linear-gradient(29deg, #5B87B5, #3B5F9E 70%)"}}>
+  <div className="container mx-auto text-center">
+    <h2 className="text-3xl lg:text-5xl font-semibold text-white">Transforme a sua empresa com os nossos serviços</h2>
+    <p className="mt-4 text-white text-xl font-thin">Soluções personalizadas e pensadas no seu negócio</p>
+    <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
+      <div className="flex-1 px-3">
+        <Card>
+          <DevIcon />
+          <p className="font-semibold text-xl">Desenvolvimento</p>
+          <p className="mt-4 mb-8">
+          Planilha que consome muito seu tempo? Tantos detalhes que sempre acaba com alguma informação errada?
+Criamos ferramentas automatizadas!
+          </p>
+        </Card>
+      </div>
+      <div className="flex-1 px-3">
+        <Card>
+          <BulbIcon />
+          <p className="font-semibold text-xl">Consultoria</p>
+          <p className="mt-4">
+          Uma das melhores opções para sua empresa, permitindo contratar planos de suporte remoto de um especialista em planilhas, apresentações e documentos.
+
+          </p>
+        </Card>
+      </div>
+      <div className="flex-1 px-3">
+        <Card>
+          <ClassIcon />
+          <p className="font-semibold text-xl">Treinamento</p>
+          <p className="mt-4">
+          Treinamentos in-company para qualificar a sua equipe nas principais ferramentas do mercado, aumentar a produtividade e
+abrir portas para novas possibilidades.
+          </p>
+        </Card>
+      </div>
+    </div>
+  </div>
+</section>
+)
+
+export default SolucoesSection
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Button from '../components/Button';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import LineButton from '../components/LineButton';
-import Card from '../components/Card';
 import CustomerCard from '../components/CustomerCard';
 import LabelText from '../components/LabelText';
 import Layout from '../components/layout/Layout';
@@ -11,17 +10,16 @@ import StatsBox from '../components/StatsBox';
 import customerData from '../data/customer-data';
 import LottieWork from '../components/LottieWork'
 import SvgCharts from '../svg/SvgCharts';
-import DevIcon from '../svg/DevIcon';
-import BulbIcon from '../svg/BulbIcon';
-import ClassIcon from '../svg/ClassIcon';
 import DynamicLogo from '../svg/DynamicLogo'
+
+import SolucoesSection from '../components/SolucoesSection'
 
 export default () => (
   <Layout>
     <section className="pt-20" id="top">
       <div className="container mx-auto px-8 lg:flex items-center">
         <div className="text-center lg:text-left lg:w-1/2">
-          <div className="w-3/5">
+          <div className="w-3/5 mx-auto lg:mx-0">
             <DynamicLogo />
           </div>
           <p className="text-xl lg:text-2xl mt-6 font-light">
@@ -31,49 +29,12 @@ export default () => (
             <Button><AnchorLink className="text-white hover:text-white" href="#solucoes">Conheça as Soluções</AnchorLink></Button>
           </p>
         </div>
-        <div className="m-0 lg:w-1/2">
+        <div className="m-0 hidden lg:w-1/2 lg:block">
           <LottieWork />
         </div>
       </div>
     </section>
-    <section id="solucoes" className="my-20 py-16 pb-20 lg:mb-40 lg:mt-48" style={{background: "linear-gradient(29deg, #5B87B5, #3B5F9E 70%)"}}>
-      <div className="container mx-auto text-center">
-        <h2 className="text-3xl lg:text-5xl font-semibold text-white">Transforme a sua empresa com os nossos serviços</h2>
-        <p className="mt-4 text-white text-xl font-thin">Soluções personalizadas e pensadas no seu negócio</p>
-        <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
-          <div className="flex-1 px-3">
-            <Card>
-              <DevIcon />
-              <p className="font-semibold text-xl">Desenvolvimento</p>
-              <p className="mt-4 mb-8">
-              Planilha que consome muito seu tempo? Tantos detalhes que sempre acaba com alguma informação errada?
-Criamos ferramentas automatizadas!
-              </p>
-            </Card>
-          </div>
-          <div className="flex-1 px-3">
-            <Card>
-              <BulbIcon />
-              <p className="font-semibold text-xl">Consultoria</p>
-              <p className="mt-4">
-              Uma das melhores opções para sua empresa, permitindo contratar planos de suporte remoto de um especialista em planilhas, apresentações e documentos.
-
-              </p>
-            </Card>
-          </div>
-          <div className="flex-1 px-3">
-            <Card>
-              <ClassIcon />
-              <p className="font-semibold text-xl">Treinamento</p>
-              <p className="mt-4">
-              Treinamentos in-company para qualificar a sua equipe nas principais ferramentas do mercado, aumentar a produtividade e
-abrir portas para novas possibilidades.
-              </p>
-            </Card>
-          </div>
-        </div>
-      </div>
-    </section>
+    <SolucoesSection />
     <SplitSection
       id="ferramentas"
       primarySlot={
