028e8ca46fb9498864a9b8e888d8c2d3b856a74b
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-18 13:43:32 -0300

    Starting Customer Cards

--- src/components/CustomerCard.js
+++ src/components/CustomerCard.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const CustomerCard = ({ customer }) => (
   <Card className="mb-8">
-    <p className="text-xl font-semibold">{customer.title}</p>
+    {/* <p className="text-xl font-semibold">{customer.title}</p> */}
     <p className="mt-6">{customer.content}</p>
     <div className="flex items-center mt-8">
       <img
--- src/data/customer-data.js
+++ src/data/customer-data.js
@@ -1,10 +1,10 @@
 export default [
   {
-    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+    title: '',
     content:
-      'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
-    customerName: 'Jane Doe',
-    customerTitle: 'Director of Research and Data',
+      'Trabalhar com a Dynamic nos traz tranquilidade. A equipe atua com auto grau de profissionalismo. Os dashboards desenvolvidos para nossa operação facilitam a gestão do dia a dia. Fizemos alguns trabalhos e, certamente, faremos outros. Temos que valorizar bons prestadores e construir uma relação duradoura.',
+    customerName: 'Eber Souza',
+    customerTitle: 'Marketing, Produto e Operações na EcoAssist',
     customerImage: 'https://placeimg.com/150/150/people'
   },
   {
