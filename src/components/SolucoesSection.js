f28d9af380600fa536bbb9c7a0e31a7481284d30
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-18 15:20:55 -0300

    SolucoesSection button

--- src/components/SolucoesSection.js
+++ src/components/SolucoesSection.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 import Card from './Card';
+import LineButton from './LineButton'
+
 import DevIcon from '../svg/DevIcon';
 import BulbIcon from '../svg/BulbIcon';
 import ClassIcon from '../svg/ClassIcon';
@@ -42,6 +45,22 @@ abrir portas para novas possibilidades.
         </Card>
       </div>
     </div>
+    <AnchorLink href="#ferramentas">
+    <LineButton className="mt-12 flex mx-auto">Especialidades Dynamic
+      {/* <svg
+      aria-hidden="true"
+      data-prefix="fas"
+      data-icon="chevron-down"
+      className="pl-2 pt-1 w-6"
+      viewBox="0 0 448 512"
+    >
+      <path
+        fill="white"
+        d="M207.029 381.476L12.686 187.132c-9.373-9.373-9.373-24.569 0-33.941l22.667-22.667c9.357-9.357 24.522-9.375 33.901-.04L224 284.505l154.745-154.021c9.379-9.335 24.544-9.317 33.901.04l22.667 22.667c9.373 9.373 9.373 24.569 0 33.941L240.971 381.476c-9.373 9.372-24.569 9.372-33.942 0z"
+      />
+    </svg> */}
+    </LineButton>
+    </AnchorLink>
   </div>
 </section>
 )
