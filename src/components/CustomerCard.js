36b677cfa226b364fdc98af3d0a716867eca6447
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-18 15:20:42 -0300

    Customer card design

--- src/components/CustomerCard.js
+++ src/components/CustomerCard.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Card from './Card';
 
 const CustomerCard = ({ customer }) => (
-  <Card className="mb-8">
+  <Card className="mb-8 flex flex-col">
     {/* <p className="text-xl font-semibold">{customer.title}</p> */}
-    <p className="mt-6">{customer.content}</p>
-    <div className="flex items-center mt-8">
+    <p className="mt-6 text-lg font-light">{customer.content}</p>
+    <div className="flex items-center mt-auto">
       <img
         className="w-12 h-12 mr-4 rounded-full"
         src={customer.customerImage}
