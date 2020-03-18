7a48ba3188db11b67467099d1a4d713965b7eac0
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-18 15:21:41 -0300

    Customer data update

--- src/data/customer-data.js
+++ src/data/customer-data.js
@@ -5,22 +5,22 @@ export default [
       'Trabalhar com a Dynamic nos traz tranquilidade. A equipe atua com auto grau de profissionalismo. Os dashboards desenvolvidos para nossa operação facilitam a gestão do dia a dia. Fizemos alguns trabalhos e, certamente, faremos outros. Temos que valorizar bons prestadores e construir uma relação duradoura.',
     customerName: 'Eber Souza',
     customerTitle: 'Marketing, Produto e Operações na EcoAssist',
-    customerImage: 'https://placeimg.com/150/150/people'
+    customerImage: 'https://ecoassist.com.br/wp-content/uploads/2019/06/fav.png'
   },
   {
     title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
     content:
-      'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
-    customerName: 'John Doe',
-    customerTitle: 'Director of Research and Data',
-    customerImage: 'https://placeimg.com/150/150/people'
+      'Conheci a Dynamic quando precisei de uma solução sob medida no Excel. Contactei a empresa e desde então muitos outros projetos surgiram. Uma parceria que traz solução a muitas áreas da tecnologia. Recomendo completamente os serviços.',
+    customerName: 'Rafael Mengue',
+    customerTitle: 'Gerente na VIP Promotora de Crédito',
+    customerImage: 'https://lh4.googleusercontent.com/-A2_aqZH1nlc/AAAAAAAAAAI/AAAAAAAAAAA/OBaDi6x1APs/s44-p-k-no-ns-nd/photo.jpg'
   },
   {
     title: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
     content:
-      'Eu lobortis elementum nibh tellus molestie nunc non blandit massa. Sit amet consectetur adipiscing elit duis.',
-    customerName: 'Jane Smith',
-    customerTitle: 'Director of Research and Data',
-    customerImage: 'https://placeimg.com/150/150/people'
+      'Quando o assunto é Microsoft sabemos que a Dynamic vai saber resolver. Não conheço nenhuma empresa mais capacitada que eles na área. Desenvolveu meu sistema completo e dashboard para controle, por um preço muito barato.',
+    customerName: 'Rafael Muttoni',
+    customerTitle: 'CTO na Muttoni Digital',
+    customerImage: 'https://muttonidigital.com/images/logo.svg'
   }
 ];
