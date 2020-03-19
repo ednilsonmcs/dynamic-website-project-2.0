884f495a06cc40e055a3ee6f9b4d0be7931bbab1
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-19 14:07:53 -0300

    Excel and Power BI SVGs

---
+++ src/svg/SvgSheets.js
@@ -0,0 +1,295 @@
+import React from 'react'
+
+const Excel = () => (
+  <div className="w-full">
+    <svg viewBox="0 0 261 154" overflow="hidden">
+      <defs>
+        <radialGradient
+          cx={2466980}
+          cy={161925}
+          r={2472280}
+          fx={2466970}
+          fy={161925}
+          gradientUnits="userSpaceOnUse"
+          spreadMethod="pad"
+          id="excelPrefix__b"
+          gradientTransform="matrix(.0001 0 0 .0001 75 30)"
+        >
+          <stop offset={0} stopColor="#16AD6C" />
+          <stop offset={0.99} stopColor="#015744" />
+          <stop offset={1} stopColor="#015744" />
+        </radialGradient>
+        <clipPath id="excelPrefix__a">
+          <path d="M74 29h261v154H74z" fillRule="evenodd" clipRule="evenodd" />
+        </clipPath>
+      </defs>
+      <g clipPath="url(#excelPrefix__a)" transform="translate(-74 -29)">
+        <path
+          d="M80.71 30H328.29A5.711 5.711 0 01334 35.71V181H75V35.71A5.71 5.71 0 0180.71 30z"
+          fill="#FFF"
+          fillRule="evenodd"
+        />
+        <path
+          d="M80.428 30h248.144A5.428 5.428 0 01334 35.428V47H75V35.428A5.428 5.428 0 0180.428 30z"
+          fill="url(#excelPrefix__b)"
+          fillRule="evenodd"
+        />
+        <path
+          d="M302 39c0-1.105 1.119-2 2.5-2s2.5.895 2.5 2-1.119 2-2.5 2-2.5-.895-2.5-2zM311 39a2 2 0 114 0 2 2 0 01-4 0zM319 39c0-1.105 1.119-2 2.5-2s2.5.895 2.5 2-1.119 2-2.5 2-2.5-.895-2.5-2z"
+          fill="#118858"
+          fillRule="evenodd"
+        />
+        <path fill="#DBDBDB" d="M75 47h259v10H75z" />
+        <path
+          d="M109 47v134.317M147 47v134.317M185 47v134.317M223 47v134.317M299 47v134.317M261 47v134.317M75 68h258.867M75 84h258.867M75 164h258.867M75 132h258.867M75 100h258.867M75 116h258.867M75 148h258.867"
+          stroke="#C4C4C4"
+          strokeWidth={2}
+          strokeMiterlimit={8}
+          fill="none"
+        />
+        <path
+          d="M109.5 47.5v10.066M147.5 47.5v10.066M185.5 47.5v10.066M223.5 47.5v10.066M299.5 47.5v10.066M261.5 47.5v10.066"
+          stroke="#A6A6A6"
+          strokeMiterlimit={8}
+          fill="none"
+        />
+        <path
+          stroke="#C4C4C4"
+          strokeMiterlimit={8}
+          fill="#D3F4E6"
+          d="M147.5 68.5h38v16h-38zM261.5 132.5h38v16h-38zM109.5 148.5h38v16h-38z"
+        />
+      </g>
+    </svg>
+  </div>
+)
+
+const PowerBI = () => (
+  <div className="w-full">
+  <svg viewBox="0 0 261 152" overflow="hidden">
+  <defs>
+    <clipPath id="PowerBIprefix__a">
+      <path
+        d="M117 55h261v152H117z"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      />
+    </clipPath>
+    <clipPath id="PowerBIprefix__c">
+      <path d="M0 0h168v105H0z" fillRule="evenodd" clipRule="evenodd" />
+    </clipPath>
+    <clipPath id="PowerBIprefix__e">
+      <path
+        d="M181 126h122v58H181z"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      />
+    </clipPath>
+    <clipPath id="PowerBIprefix__f">
+      <path
+        d="M181 126h122v58H181z"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      />
+    </clipPath>
+    <clipPath id="PowerBIprefix__g">
+      <path
+        d="M181 126h122v58H181z"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      />
+    </clipPath>
+    <clipPath id="PowerBIprefix__h">
+      <path d="M0 0h93v53H0z" fillRule="evenodd" clipRule="evenodd" />
+    </clipPath>
+    <clipPath id="PowerBIprefix__j">
+      <path d="M287 151h75v35h-75z" fillRule="evenodd" clipRule="evenodd" />
+    </clipPath>
+    <clipPath id="PowerBIprefix__k">
+      <path d="M287 151h75v35h-75z" fillRule="evenodd" clipRule="evenodd" />
+    </clipPath>
+    <clipPath id="PowerBIprefix__l">
+      <path d="M287 151h75v35h-75z" fillRule="evenodd" clipRule="evenodd" />
+    </clipPath>
+    <clipPath id="PowerBIprefix__m">
+      <path d="M0 0h101v73H0z" fillRule="evenodd" clipRule="evenodd" />
+    </clipPath>
+    <clipPath id="PowerBIprefix__o">
+      <path d="M129 94h84v55h-84z" fillRule="evenodd" clipRule="evenodd" />
+    </clipPath>
+    <clipPath id="PowerBIprefix__p">
+      <path d="M129 94h84v55h-84z" fillRule="evenodd" clipRule="evenodd" />
+    </clipPath>
+    <clipPath id="PowerBIprefix__q">
+      <path d="M129 94h84v55h-84z" fillRule="evenodd" clipRule="evenodd" />
+    </clipPath>
+    <clipPath id="PowerBIprefix__r">
+      <path d="M129 94h84v55h-84z" fillRule="evenodd" clipRule="evenodd" />
+    </clipPath>
+    <image
+      width={168}
+      height={105}
+      xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAKgAAABpCAMAAACZIZiXAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABRUExURQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFypFUIAAAAbdFJOUwABAgMEBQYHCAkKCwwNDg8QERITFBUWFxgZGomogSgAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAEnSURBVHhe7dtJjoNAEERRpmIyZQzGDL7/QTulRhat9jIWWdL/J3iKfWREiZS77QT+ludFUZQOM9aVas6yCrXDQlVepeYMTdv17uraJpj0ZNqg5uyHeB+ddY9Db9LPpHlR1X0cp/nprHkaY19Xn0kN2tzGeVndtczjrblCQzs8lnXbnbWty2Nowx9onF7bfjhr315T/AI9jrerjuM/tDPo7sxp0t2gHVBdQNUBVQdUHVB1QNUBVQdUHVB1QNUBVQdUHVB1QNUBVQdUHVB1QNUBVQdUHVB1QNUBVQdUHVB1QNUBVQdUHVB1QNUBVQdUHVB1QNUBVQdUHVB1QNV9gSbzvknnz5TIQyyZz10qL8ZUfqE2aRpP23S+y8m8wa3zzO6wE0jkuyz7AVrMYVKok2f7AAAAAElFTkSuQmCC"
+      preserveAspectRatio="none"
+      id="PowerBIprefix__d"
+    />
+    <image
+      width={93}
+      height={53}
+      xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAF0AAAA1CAMAAAAUCBfGAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABRUExURQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFypFUIAAAAbdFJOUwABAgMEBQYHCAkKCwwNDg8QERITFBUWFxgZGomogSgAAAAJcEhZcwAADsMAAA7DAcdvqGQAAADdSURBVFhH7dhND4MgDIBhEQXxW0R0+/8/dNWxRN2aHaC3vkdinhBPLRn3L5GyYH6CkzxdN3+3ZZEquftB3hNgK62rFGmtwD/pQshSm6ZNU2N0KU+XF3mhTNuP4xTfOPatUcXp8qBXzWBnt8TnZjs01UWXpekm59cUeTd1Bn5NsN96b5d1e8S3rYvtb7oC3QP+jA14D7q66vWhh09iOvT6S5+T6TPrv2Idi3Us1rFYx2Idi3Us1rEQnXIWI50jaWdg0vmddveg3ZtIdz7afZV416Z9J4DgJF3B5JCy7AUPaiUYu1gJoQAAAABJRU5ErkJggg=="
+      preserveAspectRatio="none"
+      id="PowerBIprefix__i"
+    />
+    <image
+      width={101}
+      height={73}
+      xlinkHref="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAGUAAABJCAMAAAATzBiwAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAABRUExURQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFypFUIAAAAbdFJOUwABAgMEBQYHCAkKCwwNDg8QERITFBUWFxgZGomogSgAAAAJcEhZcwAADsMAAA7DAcdvqGQAAADoSURBVFhH7dnLDoMgEIVhLyCIIl4o2L7/g3ZsaFJNu5hkJCadb0nM/Ptjwa6tPEO6/QYvFb1DBxq1EJKWEDV0UmEDEdkorXVLBW6pRkImFcAWUabrLaW+M2qXKSuhjHXjRGl01iixq0jdu9nfKPnZ9Vp+VuqmtZMPMa5UYgx+sm1T7ypmmENc73TWGObBHCtuCRB5UIFMWNz3SvqEAlewuILFFSyuYHEFiytYXMHiChZXsLiCxRUsrmBxBYsrWH9Socv82JSy7GM5tr48u2WmDTbHnvzKnL6NZ9r58/yzAPBCL91ml1QUT9xA4VaYQ23+AAAAAElFTkSuQmCC"
+      preserveAspectRatio="none"
+      id="PowerBIprefix__n"
+    />
+    <radialGradient
+      cx={2466980}
+      cy={161925}
+      r={2472280}
+      fx={2466970}
+      fy={161925}
+      gradientUnits="userSpaceOnUse"
+      spreadMethod="pad"
+      id="PowerBIprefix__b"
+      gradientTransform="matrix(.0001 0 0 .0001 118 55)"
+    >
+      <stop offset={0} stopColor="#FFE699" />
+      <stop offset={0.28} stopColor="#EDBD11" />
+      <stop offset={1} stopColor="#EDBD11" />
+    </radialGradient>
+  </defs>
+  <g clipPath="url(#PowerBIprefix__a)" transform="translate(-117 -55)">
+    <path
+      d="M123.711 55h247.578A5.711 5.711 0 01377 60.71V206H118V60.71a5.711 5.711 0 015.711-5.71z"
+      fill="#FFF"
+      fillRule="evenodd"
+    />
+    <path
+      d="M123.428 55h248.144A5.428 5.428 0 01377 60.428V72H118V60.428A5.428 5.428 0 01123.428 55z"
+      fill="url(#PowerBIprefix__b)"
+      fillRule="evenodd"
+    />
+    <path
+      d="M346 64.5c0-1.38.895-2.5 2-2.5s2 1.12 2 2.5-.895 2.5-2 2.5-2-1.12-2-2.5zM354 64.5a2.5 2.5 0 115 0 2.5 2.5 0 01-5 0zM363 64.5c0-1.38.895-2.5 2-2.5s2 1.12 2 2.5-.895 2.5-2 2.5-2-1.12-2-2.5z"
+      fill="#212121"
+      fillRule="evenodd"
+    />
+    <path fill="#F3F2F1" d="M118 72h259v17H118z" />
+    <g clipPath="url(#PowerBIprefix__c)" transform="translate(158 102)">
+      <use width="100%" height="100%" xlinkHref="#PowerBIprefix__d" />
+    </g>
+    <path fill="#FFF" d="M167 111h150v87H167z" />
+    <g clipPath="url(#PowerBIprefix__e)">
+      <path
+        d="M188 159h5v24h-5zm30 10h5v14h-5zm30-6h5v20h-5zm30-6h5v26h-5z"
+        fill="#CBCBCB"
+      />
+    </g>
+    <g clipPath="url(#PowerBIprefix__f)">
+      <path
+        d="M194 169h5v14h-5zm30-11h6v25h-6zm31 15h5v10h-5zm30-6h5v16h-5z"
+        fill="#A5A5A5"
+      />
+    </g>
+    <g clipPath="url(#PowerBIprefix__g)">
+      <path
+        d="M201 172h5v11h-5zm30 0h5v11h-5zm30-6h6v17h-6zm30-11h6v28h-6z"
+        fill="#888"
+      />
+    </g>
+    <g clipPath="url(#PowerBIprefix__h)" transform="translate(279 142)">
+      <use width="100%" height="100%" xlinkHref="#PowerBIprefix__i" />
+    </g>
+    <path fill="#FFF" d="M288 150h75v37h-75z" />
+    <g clipPath="url(#PowerBIprefix__j)">
+      <path
+        d="M297.5 171.5l18 7 19-4 19-4"
+        stroke="#CBCBCB"
+        strokeWidth={3}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        fill="none"
+      />
+    </g>
+    <g clipPath="url(#PowerBIprefix__k)">
+      <path
+        d="M297.5 178.5l18-7 19 9 19-4"
+        stroke="#A5A5A5"
+        strokeWidth={3}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        fill="none"
+      />
+    </g>
+    <g clipPath="url(#PowerBIprefix__l)">
+      <path
+        d="M297.5 179.5h18l19-3 19-7"
+        stroke="#888"
+        strokeWidth={3}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        fill="none"
+      />
+    </g>
+    <g clipPath="url(#PowerBIprefix__m)" transform="translate(121 86)">
+      <use width="100%" height="100%" xlinkHref="#PowerBIprefix__n" />
+    </g>
+    <path fill="#FFF" d="M130 95h83v55h-83z" />
+    <g clipPath="url(#PowerBIprefix__o)">
+      <path
+        d="M171.291 109.274c7.067 0 12.796 5.729 12.796 12.796 0 7.068-5.729 12.797-12.796 12.797a12.79 12.79 0 01-6.563-1.812l6.563-10.985z"
+        stroke="#FFF"
+        strokeWidth={2}
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        fill="#D1D1D1"
+        fillRule="evenodd"
+      />
+    </g>
+    <g clipPath="url(#PowerBIprefix__p)">
+      <path
+        d="M164.728 133.055a12.797 12.797 0 01-5.204-16.014l11.767 5.029z"
+        stroke="#FFF"
+        strokeWidth={2}
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        fill="#B5B5B5"
+        fillRule="evenodd"
+      />
+    </g>
+    <g clipPath="url(#PowerBIprefix__q)">
+      <path
+        d="M159.524 117.041a12.794 12.794 0 015.204-5.955l6.563 10.984z"
+        stroke="#FFF"
+        strokeWidth={2}
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        fill="#9A9A9A"
+        fillRule="evenodd"
+      />
+    </g>
+    <g clipPath="url(#PowerBIprefix__r)">
+      <path
+        d="M164.728 111.086a12.79 12.79 0 016.563-1.812v12.796z"
+        stroke="#FFF"
+        strokeWidth={2}
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        fill="#818181"
+        fillRule="evenodd"
+      />
+    </g>
+  </g>
+</svg>
+</div>
+)
+
+export {Excel, PowerBI}
