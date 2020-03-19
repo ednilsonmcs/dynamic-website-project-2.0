bbadfb927cc924cdffb066673c20d93374ab6558
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-19 15:19:03 -0300

    Progress in Planilhas Gratis

--- src/components/PlaninhaSection.js
+++ src/components/PlaninhaSection.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import Button from './Button'
 
-const PlanilhasSection = ({id, title, description, img, reverseOrder}) => (
-  <div className="py-12" key={id}>
-    <div className="container mx-auto px-16 items-center flex flex-col lg:flex-row">
+const PlanilhasSection = ({id, title, description, img, downloadLink, reverseOrder}) => (
+  <div className="py-12 mb-18" key={id}>
+    <div 
+      className={`container mx-auto px-16 items-center flex flex-col justify-between lg:flex-row border-primary  ${reverseOrder ? `border-r-2` : `border-l-2`}`}
+      >
       <div className="lg:w-1/2">
-        <h3 className="text-center text-4xl">{title}</h3>
-        <p className="text-center font-md">{description}</p>
+        <h3 className={`text-5xl ${reverseOrder ? `text-right` : `text-left `}`}>{title}</h3>
+        <p className={`front-md ${reverseOrder ? `text-right` : `text-left `}`}>{description}</p>
+        <a href={downloadLink} target="_blank"><Button className={`flex mt-4 ${reverseOrder ? 'ml-auto' : 'mr-auto'}`}>FAZER DOWNLOAD DA PLANILHA</Button></a>
       </div>
       <div
         className={`mt-10 lg:mt-0 w-1/2 lg:w-1/3 ${reverseOrder && `order-last lg:order-first`}`}
       >
-        <img src={img} />
+        <img className="ml-auto" src={img} />
       </div>
     </div>
   </div>
--- src/data/planilhas-data.js
+++ src/data/planilhas-data.js
@@ -1,9 +1,18 @@
 export default [
   {
-    title:'Planilha 1',
-    description:'Top top top',
+    title:'Planilha Gestão Financeira',
+    description:'A segunda planilha gratuita desse ano da Dynamic Soluções é para controlar as finanças pessoais. Ela possui um Dashboard interativo por datas, cadastro de metas e lançamentos com o uso facilitado e intuitivo.',
     img:'https://placeimg.com/640/480/tech',
+    downloadLink: 'https://drive.google.com/u/0/uc?id=1V2ymiTrm8h1v3ed8Q7we7AjtOXiAnAAa&export=download',
     reverseOrder: false,
+    id: 1
+  },
+  {
+    title:'Planilha Gestão Financeira',
+    description:'A segunda planilha gratuita desse ano da Dynamic Soluções é para controlar as finanças pessoais. Ela possui um Dashboard interativo por datas, cadastro de metas e lançamentos com o uso facilitado e intuitivo.',
+    img:'https://placeimg.com/640/480/tech',
+    downloadLink: 'https://drive.google.com/u/0/uc?id=1V2ymiTrm8h1v3ed8Q7we7AjtOXiAnAAa&export=download',
+    reverseOrder: true,
     id: 1
   },
 ]
