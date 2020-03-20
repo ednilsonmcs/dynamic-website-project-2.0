0d82287d46fde5fd08fb81179e3c53269c491a10
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-20 14:08:07 -0300

    Logo section responsive

--- src/components/LogoSection.js
+++ src/components/LogoSection.js
@@ -4,15 +4,15 @@ import {EcoAssist, EngeSafety, Grasiele, Lavorolab, Martiplast, Orella, Otimizar
 
 const LogoSection = () => (
     <section className="grid grid-cols-3 gap-16 py-20 mx-auto container">
-      <div className="flex mx-auto"><EcoAssist className="w-40 m-auto"/></div>
-      <div className="flex mx-auto"><EngeSafety className="w-40 m-auto"/></div>
-      <div className="flex mx-auto"><Grasiele className="w-40 m-auto"/></div>
-      <div className="flex mx-auto"><Lavorolab className="w-40 m-auto"/></div>
-      <div className="flex mx-auto"><Martiplast className="w-32 m-auto"/></div>
-      <div className="flex mx-auto"><Orella className="w-40 m-auto"/></div>
-      <div className="flex mx-auto"><Otimizare className="w-40 m-auto"/></div>
-      <div className="flex mx-auto"><Sugarshoes className="w-40 m-auto"/></div>
-      <div className="flex mx-auto"><Vigitec className="w-1/5 m-auto"/></div> 
+      <div className="flex mx-auto"><EcoAssist className="w-24 md:w-40 m-auto"/></div>
+      <div className="flex mx-auto"><EngeSafety className="w-32 md:w-40 m-auto"/></div>
+      <div className="flex mx-auto"><Grasiele className="w-24 md:w-40 m-auto"/></div>
+      <div className="flex mx-auto"><Lavorolab className="w-32 md:w-40 m-auto"/></div>
+      <div className="flex mx-auto"><Martiplast className="w-24 md:w-32 m-auto"/></div>
+      <div className="flex mx-auto"><Orella className="w-32 md:w-40 m-auto"/></div>
+      <div className="flex mx-auto"><Otimizare className="w-24 md:w-40 m-auto"/></div>
+      <div className="flex mx-auto"><Sugarshoes className="w-32 md:w-40 m-auto"/></div>
+      <div className="flex mx-auto"><Vigitec className="w-1/4 lg:w-1/5 m-auto"/></div> 
     </section>
 )
 
