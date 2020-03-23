8a1573c85bc9829a10c27c6bbf28a6f9551b487a
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-23 14:04:19 -0300

    image card

--- src/components/seo.js
+++ src/components/seo.js
@@ -30,6 +30,7 @@ function SEO({ description, lang, meta, title, image }) {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
+      image={image}
       meta={[
         {
           name: `description`,
