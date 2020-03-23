687d7148999b28c79350ff637a63d417c1fab74f
Rafael Muttoni <rmuttonic@gmail.com>
2020-03-23 14:12:20 -0300

    changed to png

--- src/components/seo.js
+++ src/components/seo.js
@@ -23,7 +23,7 @@ function SEO({ description, lang, meta, title, image }) {
 
   const url = site.siteMetadata.siteUrl
 
-  const ogImage = `${url}/assets/img/dynamic.jpeg`
+  const ogImage = `${url}/assets/img/dynamic.png`
 
   return (
     <Helmet
