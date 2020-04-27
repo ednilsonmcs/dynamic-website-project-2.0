20d09adf2457524c08c68e9de994c5e188d94170
rodnr <64420738+rodnr@users.noreply.github.com>
2020-04-27 12:01:00 -0300

    Update planilhas-gratuitas.js
    Alteração de texto

--- src/pages/planilhas-gratuitas.js
+++ src/pages/planilhas-gratuitas.js
@@ -10,7 +10,7 @@ const PlanilhasGratuitasPage = () => (
     <SEO title="Planilhas Gratuitas" description="Todo mês uma planilha nova 100% gratuita para você aproveitar!"/>
     <div className="mb-16" style={{background: "linear-gradient(29deg, #5B87B5, #3B5F9E 70%)"}}>
       <h1 className="text-white text-6xl text-center font-bold pt-24">Planilhas Gratuitas Dynamic</h1>
-      <p className="text-white text-xl text-center font-light pt-2 pb-24">Feitas especialmente para você.</p>
+      <p className="text-white text-xl text-center font-light pt-2 pb-24">Feitas especialmente para você, só baixar e sair usando!</p>
     </div>
     {planilhasData.map(data => (
       <PlanilhasSection id={data.id} title={data.title} description={data.description} img={data.img} downloadLink={data.downloadLink} reverseOrder={data.reverseOrder}  />
